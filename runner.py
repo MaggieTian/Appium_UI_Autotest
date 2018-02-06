2d97fdce496a30e418f1b2df78573379f76187d7
maggie <qi.tian@zongmutech.com>
2018-02-06 17:32:00 +0800

    resolve can not change in Test dir by using os.popen('cd path')

--- runner.py
+++ runner.py
@@ -10,6 +10,7 @@ import os
 import logging
 
 command = "behave"
+root_path =os.path.abspath(os.path.dirname(__file__)) # 得到项目根目录
 # 从配置文件中读取运行的tag,若没有tag则运行全部feature
 tree = ET.parse(os.path.join(LogHelper.config_file_path, "run_config.xml"))
 root = tree.getroot()
@@ -17,10 +18,13 @@ if root:
     for node in root:
         if node.text:
             command += " --tags={}".format(node.text)
-command += r" >out.txt"
+command += r" >{path}{sep}{filename}".format(path=root_path+os.sep+"Log", sep=os.sep, filename="out.txt")
 print(command)
 logging.info("run command is {cmd}".format(cmd=command))
 
-root_path = os.path.dirname(__file__)
-os.popen("cd {path}".format(path=os.path.join(root_path,os.sep+"Test")))
-# os.popen(command)
+
+test_path = root_path+os.sep+"Test"
+os.chdir(test_path)
+p = os.popen("dir")
+print(p.read())
+os.popen(command)
